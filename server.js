e9085a05729af530ebae6bc39e75a2c256ec0309
AthenaX3 <48192464+AthenaX3@users.noreply.github.com>
2019-04-26 10:37:08 -0400

    Add files via upload
    "Before each hook" testing error

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const server = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
+const morgan = require('morgan');
+const apiRouter = require('./api/api');
+
+
+const PORT = process.env.PORT || 4000;
+
+server.use(bodyParser.json());
+server.use(cors());
+server.use(errorhandler());
+//server.use(morgan('dev'));
+server.use('/api', apiRouter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+server.listen(PORT, () => {
+  console.log('Listening on port: ' + PORT);
+});
+
+module.exports = server;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = server;
